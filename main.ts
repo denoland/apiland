150f9b83bf2e2686fe19d590a163db30392e47cb
Kitson Kelly <me@kitsonkelly.com>
2022-05-05 15:22:51 +1000

    initial commit

---
+++ main.ts
@@ -0,0 +1,38 @@
+import { Router } from "https://deno.land/x/acorn@0.0.1/mod.ts";
+
+const router = new Router();
+
+router.all("/", () => {
+  return new Response(
+    `<!DOCTYPE html>
+  <html>
+    <head>
+      <title>api.deno.land</title>
+    </head>
+    <body>
+      <h1>api.deno.land</h1>
+    </body>
+  </html>`,
+    {
+      headers: {
+        "content-type": "text/html",
+      },
+    },
+  );
+});
+
+router.get("/ping", () => {
+  return { pong: true };
+});
+
+router.addEventListener("listen", (evt) => {
+  console.log(
+    `%cListening: %c${
+      evt.secure ? "https://" : "http://"
+    }${evt.hostname}:${evt.port}`,
+    "color:green;font-weight:bold;",
+    "color:yellow",
+  );
+});
+
+router.listen({ port: 3000 });
