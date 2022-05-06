7e51c43007e6d8e7c3d2f7f570098877113b5fa5
Kitson Kelly <me@kitsonkelly.com>
2022-05-06 18:21:58 +1000

    feat: mock out v2 APIs

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 
-import { Router } from "https://deno.land/x/acorn@0.0.2/mod.ts";
+import { Router } from "https://deno.land/x/acorn@0.0.3/mod.ts";
+import modulesDB from "./mocks/modules.json" assert { type: "json" };
+import modulesVersionsDB from "./mocks/modules_versions.json" assert {
+  type: "json",
+};
+
+type ModuleKey = keyof typeof modulesDB;
+type VersionsKey<Key extends ModuleKey> = keyof (typeof modulesVersionsDB)[Key];
 
 const router = new Router();
 
@@ -23,6 +30,20 @@ router.all("/", () => {
   );
 });
 
+router.get("/v2/modules", () => Object.values(modulesDB));
+router.get(
+  "/v2/modules/:id",
+  (ctx) => modulesDB[ctx.params.id as ModuleKey],
+);
+router.get(
+  "/v2/modules/:id/:version",
+  (ctx) => {
+    return modulesVersionsDB[ctx.params.id as ModuleKey][
+      ctx.params.version as VersionsKey<ModuleKey>
+    ];
+  },
+);
+
 router.get("/ping", () => ({ pong: true }));
 
 router.addEventListener("listen", (evt) => {
@@ -32,6 +53,26 @@ router.addEventListener("listen", (evt) => {
     }${evt.hostname}:${evt.port}`,
     "color:green;font-weight:bold;",
     "color:yellow",
+  );
+});
+
+router.addEventListener("handled", (evt) => {
+  const responseColor = evt.response.status < 400
+    ? "color:green"
+    : evt.response.status < 500
+    ? "color:yellow"
+    : "color:red";
+  let url;
+  try {
+    url = new URL(evt.request.url);
+  } catch {
+    // just swallow errors here
+  }
+  console.log(
+    `%c${evt.request.method} ${
+      evt.route?.route ?? url?.pathname
+    } - [${evt.response.status}] ${evt.measure.duration.toFixed(2)}ms`,
+    responseColor,
   );
 });
 
