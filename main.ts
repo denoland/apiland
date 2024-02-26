be3a69629abdc5a4544db7707d0762a447d7a178
crowlkats <crowlkats@toaxl.com>
2024-02-26 19:01:32 +0100

    fmt

--- main.ts
+++ main.ts
@@ -338,11 +338,13 @@ router.get("/v2/metrics/dependencies/:source*", async (ctx) => {
 // ## Registry related APIs ##
 router.get("/legacy_modules/:id", async (ctx) => {
   const datastore = await getDatastore();
-  const res = await datastore.lookup(datastore.key([kinds.LEGACY_MODULES, ctx.params.id]));
+  const res = await datastore.lookup(
+    datastore.key([kinds.LEGACY_MODULES, ctx.params.id]),
+  );
   if (res.found && res.found.length === 1) {
     return entityToObject<ApiModuleData>(res.found![0].entity);
   }
-})
+});
 
 router.get("/legacy_modules_count", async () => {
   datastore = datastore ?? await getDatastore();
